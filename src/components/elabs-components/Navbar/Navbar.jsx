5af3c85dc9c7fad06a0e0f7887e151bb1c8757f8
Pratyay Saha <pratyaysaha@outlook.com>
2022-09-10 22:52:00 +0530

    Navbar (#5)
    * /test/Navbar
    
    * /test/Navbar
    
    * /test/navbar-updated
    
    * test/navbar/enhanced
    
    * /test/navbar/transparent
    
    * Delete route list
    
    Co-authored-by: MohdOwais22 <mohdowais762@gmail.com>

--- src/components/elabs-components/Navbar/Navbar.jsx
+++ src/components/elabs-components/Navbar/Navbar.jsx
@@ -2,24 +2,39 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 import Logo from "../../../assets/images/elabs_logo.png";
-import { list } from "../../../assets/lists/routeList";
+import { list } from "../../../assets/lists/NavbarRouteList.js";
+// import { Button } from "@mui/material";
+
+// const ColorButton = styled(Button)(({ theme }) => ({
+//     color: "#000",
+//     backgroundColor: "#ffc000",
+//     padding: "0.5rem 1rem",
+//     "&:hover": {
+//         backgroundColor: "#ffc000",
+//     },
+// }));
 
 const Navbar = () => {
     const [offset, setOffset] = useState(0);
     const [clicked, setClicked] = useState(false);
 
-    // useEffect(() => {
-    //     const onScroll = () => setOffset(window.pageYOffset);
-    //     window.removeEventListener("scroll", onScroll);
-    //     window.addEventListener("scroll", onScroll, { passive: true });
-    //     return () => window.removeEventListener("scroll", onScroll);
-    // }, []);
+    useEffect(() => {
+        const onScroll = () => setOffset(window.pageYOffset);
+        window.removeEventListener("scroll", onScroll);
+        window.addEventListener("scroll", onScroll, { passive: true });
+        return () => window.removeEventListener("scroll", onScroll);
+    }, []);
     // console.log(offset);
 
     return (
-        <nav className="NavbarItems fixed">
-            {/* <nav className={`NavbarItems ${offset > "400" ? "fixed" : ""}`}> */}
-            <h1 className="navbar-logo">
+        <nav className={`NavbarItems ${offset > "400" ? "fixed" : ""}`}>
+            <h1
+                className={
+                    window.location.pathname === "/"
+                        ? "navbar-logo-transparent"
+                        : "navbar-logo"
+                }
+            >
                 <img src={Logo} height="42" alt="nothing" />E Labs
             </h1>
             <div className="menu-icon" onClick={() => setClicked(!clicked)}>
@@ -39,6 +54,9 @@ const Navbar = () => {
             <button>
                 <h3>Login</h3>
             </button>
+            {/* <ColorButton variant="contained">
+                <h3>Login</h3>
+            </ColorButton> */}
         </nav>
     );
 };
