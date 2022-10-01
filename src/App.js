17b7bdf5e9e3f7288dfbd536c4af76d301bdc719
pratyaysaha <pratyaysaha@outlook.com>
2022-10-01 13:57:05 +0530

    Revert "Merge branch 'master' of https://github.com/Kiit-Elabs/elabs-web-v2 into master"
    This reverts commit a5dde3a89fd032d4c746dc1aa06bf201c43a7eea, reversing
    changes made to 978d149ae5009febb6a6b429bae4bbbb7e0a6359.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import "./App.css";
-import Navbar from "./components/elabs-components/Navbar/Navbar.jsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Team from "./pages/teams/Team.jsx";
+import FooterSmall from "./components/elabs-components/Footer/FooterSmall.jsx";
+import Login from "./pages/Login/Login.jsx";
+import Navbar from "./components/elabs-components/Navbar/Navbar.jsx";
 
 function App() {
     return (
         <>
             <Router>
                 <Navbar />
-                <div className="main-app">
+                <div>
                     <Routes>
                         <Route path="/" element={<Home />} />
                         <Route path="/team" element={<Team />} />
                         <Route
                             path="/course"
-                            element={<div>Course page</div>}
+                            element={
+                                <div className="main-app">Course page</div>
+                            }
                         />
+                        <Route path="/dashboard">
+                            <Route path="login" element={<Login />} />
+                        </Route>
                     </Routes>
                 </div>
+                <FooterSmall />
             </Router>
         </>
     );
